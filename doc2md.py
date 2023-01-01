16b7ffe5ae1c9ed1eed746f2037bdee54a7383bc
lovelyzoo <g_haslett@yahoo.co.uk>
2023-01-01 12:53:59 +0000

    accept a file parameter

--- doc2md.py
+++ doc2md.py
@@ -1,16 +1,13 @@
+import argparse
 from markdownify import markdownify as md
 from bs4 import BeautifulSoup
 import re
 
-# Read the html file into bs4 object
-with open('TUDEBconversion.html', 'r') as f:
-	soup = BeautifulSoup(f.read(), 'html.parser')
-	
-# remove the style element
-soup.find('style').clear()
-  
-# convert html to markdown
-as_md = md(str(soup), heading_style="SETEXT", strip=['hr'], bullets = '-')
+# TODO:
+# Input file as parameter
+# Output file as parameter
+# Get title from start of file
+# Make preamble optional
 
 preamble = '''---
 layout: default
@@ -22,10 +19,29 @@ title: Through Ultan's Door One
 
 # '''
 
-with open('TUD_one.md', 'w') as f:
-	f.write(preamble)
+if __name__ == "__main__":
 
-	for line in as_md.splitlines():
-		f.write(re.sub(r'\A#', r'##', line))
-		f.write('\n')
+	parser = argparse.ArgumentParser(
+                    prog = 'doc2md',
+                    description = 'Converts a google doc, exported as an html file, to markdown')
+	
+	parser.add_argument('infile')
+	
+	args = parser.parse_args()
+	
+	# Read the html file into bs4 object
+	with open(args.infile, 'r') as f:
+		soup = BeautifulSoup(f.read(), 'html.parser')
 
+	# remove the style element
+	soup.find('style').clear()
+	  
+	# convert html to markdown
+	as_md = md(str(soup), heading_style="SETEXT", strip=['hr'], bullets = '-')
+	
+	with open('TUD_one.md', 'w') as f:
+		f.write(preamble)
+
+		for line in as_md.splitlines():
+			f.write(re.sub(r'\A#', r'##', line))
+			f.write('\n')
